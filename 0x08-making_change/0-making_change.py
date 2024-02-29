40089a28893ddcb865316cf4077444cc42e38201
Delight Sheriff <delightsheriff@gmail.com>
2024-02-29 20:57:31 +0100

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+    coins = sorted(coins, reverse=True)
+    count = 0
+    coin_sum = 0
+    for coin in coins:
+        while coin  + coin_sum<= total:
+            coin_sum += coin
+            count += 1
+        if coin_sum == total:
+            return count
+    if coin_sum == total:
+        return count
+    return -1
